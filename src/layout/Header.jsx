44e9de8a00aac24dcad7665a4370769361dedb5d
GeniePrinie <geniexpn@gmail.com>
2024-03-12 21:25:34 +0100

    Add Header and styles

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,11 +2,37 @@ import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header>
-      <NavLink to={"/"}>Hjem</NavLink>
-      <NavLink to={"/fysioterapeuter"}>Fysioterapeuter</NavLink>
-      <NavLink to={"/kontakt/bestillingtime"}>Bestilling Time</NavLink>
-      <NavLink to={"/kontakt/adresse"}>Adresse</NavLink>
+    <header className="bg-black">
+      <div className="d-flex mx-3 pt-3 mb-4 container">
+        <img
+          src="/src/assets/logo.png"
+          alt="holten-institutt-logo"
+          className="logo me-5"
+        />
+        <div className=" text-white align-self-center logo-text">
+          fysioterapi
+        </div>
+      </div>
+      <nav className="text-center fs-3 pb-4">
+        <NavLink
+          to={"/"}
+          className={({ isActive }) => (isActive ? "mx-4 link-active" : "mx-4")}
+        >
+          Hjem
+        </NavLink>
+        <NavLink
+          to={"/fysioterapeuter"}
+          className={({ isActive }) => (isActive ? "mx-4 link-active" : "mx-4")}
+        >
+          Fysioterapeuter
+        </NavLink>
+        <NavLink
+          to={"/kontakt"}
+          className={({ isActive }) => (isActive ? "mx-4 link-active" : "mx-4")}
+        >
+          Kontakt
+        </NavLink>
+      </nav>
     </header>
   );
 };
